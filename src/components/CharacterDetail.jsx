dcd0c0ba29fbb8e86e1bfd18bc8d6ece3f7cc87e
Patricia <patriciaparreirasainz@gmail.com>
2025-02-06 13:53:48 +0100

    responsive and route update

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,35 +1,42 @@
 import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
 import '../styles/CharacterDetail.scss'
+import PageNotFound from './PageNotFound';
 
 function CharacterDetail({characterInfo}) {
 
-    if (!characterInfo) {
-        return <p className=".CharacterDetail_loadingText">Los detalles del personaje se están cargando</p>;
+    const results = () => {
+        if (characterInfo) {
+            return <div className="CharacterDetail_body">
+                <div className="CharacterDetail_container">
+                    <Link to={'/'} className="CharacterDetail_linkBack">&lt; Volver</Link>
+                    <article className="CharacterDetail_article">
+                        <img className="CharacterDetail_img" src={characterInfo.photo}
+                        alt={characterInfo.name} />
+                        <div className="CharacterDetail_textContainer">
+                            <img className="CharacterDetail_houseEmblem" src={characterInfo.houseEmblem} alt={characterInfo.house}/>
+                            <h3>{characterInfo.name}</h3>
+                            <h4>{characterInfo.alternateNames && characterInfo.alternateNames.length > 0 ? characterInfo.alternateNames.join(', ') : 'No tiene apodos'}</h4>
+                            <div className="CharacterDetail_info">
+                                <p>Status: <span>{characterInfo.alive ? "alive 🪄" : "dead 💀"}</span></p>
+                                <p>Species: <span>{characterInfo.species}</span></p>
+                                <p>Gender: <span>{characterInfo.gender}</span></p>
+                                <p>House: <span>{characterInfo.house}</span></p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        } else if (characterInfo === null) {
+            return <p className=".CharacterDetail_loadingText">Los detalles del personaje se están cargando</p>;
+        } else if (!characterInfo){
+            return <PageNotFound/>;
+        }
     }
 
   return (
     <>
-    <div className="CharacterDetail_body">
-        <div className="CharacterDetail_container">
-            <Link to={'/'} className="CharacterDetail_linkBack">&lt; Volver</Link>
-            <article className="CharacterDetail_article">
-                <img className="CharacterDetail_img" src={characterInfo.photo}
-                alt={characterInfo.name} />
-                <div className="CharacterDetail_textContainer">
-                    <img className="CharacterDetail_houseEmblem" src={characterInfo.houseEmblem} alt={characterInfo.house}/>
-                    <h3>{characterInfo.name}</h3>
-                    <h4>{characterInfo.alternateNames && characterInfo.alternateNames.length > 0 ? characterInfo.alternateNames.join(', ') : 'No tiene apodos'}</h4>
-                    <div className="CharacterDetail_info">
-                        <p>Status: <span>{characterInfo.alive ? "alive 🪄" : "dead 💀"}</span></p>
-                        <p>Species: <span>{characterInfo.species}</span></p>
-                        <p>Gender: <span>{characterInfo.gender}</span></p>
-                        <p>House: <span>{characterInfo.house}</span></p>
-                    </div>
-                </div>
-            </article>
-        </div>
-    </div>
+    {results()}
     </>
   )
 }
